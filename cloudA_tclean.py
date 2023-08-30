6c36329275596b9da0441e57e62ba09605cff8d7
Ashley Barnes <ashleybarnes.astro@gmail.com>
2023-08-30 21:37:01 +0100

    do_sdintimaging

--- cloudA_tclean.py
+++ cloudA_tclean.py
@@ -1,19 +1,37 @@
 #### Handy functions
 def add_restoringbeamtable(imagename):
-        ia.open(imagename)
-        shape = ia.shape()
-        restoringbeam = ia.restoringbeam()
-        ia.close()
-        for i in range(shape[0]):
-                ia.open(imagename)
-                ia.setrestoringbeam(beam=restoringbeam, channel = i, polarization = 0, remove=True)
-                ia.close()
+
+       cube_info = imhead(imagename, mode='list')
+       n_chan = cube_info['shape'][-1]
+       n_pol = cube_info['shape'][-2]
+
+       ia.open(imagename)
+       restoring_beam = ia.restoringbeam()
+       ia.setrestoringbeam(remove=True)
+       for c in range(n_chan):
+              for p in range(n_pol):
+                     ia.setrestoringbeam(beam=restoring_beam, channel=c, polarization=p)
+       ia.close()
+       imhead(imagename, 'put', 'CASAMBM', 'T')
 ####
 
+
+
+
 #### 
-filename_12m7mtp = 'data/cloudA/cloudA_12m+7m+tp_n2hp10'
-filename_12m7m = 'data/cloudA/cloudA_12m+7m_n2hp10'
-filename_tp = 'data/cloudA/cloudA_tp_n2hp10'
+do_clean = False
+do_sdprep = False
+do_iramprep = False
+do_feather = False
+do_sdintimaging = True
+do_sdintimaging_iram = True
+
+input_dir = 'data/cloudA'
+filename_12m7mtp = f'{input_dir}/cloudA_12m+7m+tp_n2hp10'
+filename_12m7m = f'{input_dir}/cloudA_12m+7m_n2hp10'
+filename_tp = f'{input_dir}/cloudA_tp_n2hp10'
+filename_iram =  f'{input_dir}/cloudA_iram_n2hp10'
+filename_12m7miram = f'{input_dir}/cloudA_12m+7m+iram_n2hp10'
 
 vis=['%s.ms' %filename_12m7m] 
 cell=['0.5arcsec'] 
@@ -23,6 +41,9 @@ phasecenter='ICRS 18:26:18.2934 -012.41.27.253'
 stokes='I' 
 specmode='cube' 
 outframe='LSRK'
+nchan=50
+start=25
+width=1
 gridder='mosaic' 
 usepointing=False 
 mosweight=True
@@ -43,34 +64,32 @@ growiterations=50
 minpercentchange=1.0 
 
 usedata='sdint'
-sdimage='%s_regrid.image'%filename_tp
 sdpsf=''
 sdgain=1.0
 dishdia=12.0
-
-do_clean = False
-do_sdprep = True
-do_feather = False
-do_sdintimaging = False
 ####
 
 
+##################
 if do_clean:
 
-       rmtables([vis[0]+'.listobs'])
        listobs(vis=vis[0],
-              listfile=vis[0]+'.listobs')
+              listfile=vis[0]+'.listobs',
+              overwrite=True)
 
-       filenames_output = ['.model','.sumwt', '.image.pbcor', '.image', '.psf', '.pb', '.weight', '.residual', '.mask']
-       rmtables([filename_12m7m+x for x in filenames_output])
+       os.system(f'rm -rf {input_dir}/*_tclean*')
 
        tclean(vis=vis,
-              imagename='%s'%filename_12m7m,
+              imagename='%s_tclean'%filename_12m7m,
               cell=cell, 
               imsize=imsize,
+              restoringbeam=restoringbeam, #commonbeam, feather will not work without
               phasecenter=phasecenter,
               stokes=stokes, 
-              specmode=specmode, 
+              specmode=specmode,
+              nchan=nchan,
+              start=start,
+              width=width,
               outframe=outframe,
               gridder=gridder, 
               pblimit=pblimit, 
@@ -89,45 +108,163 @@ if do_clean:
               growiterations=growiterations, 
               minpercentchange=minpercentchange)
 
+
+##################
 if do_sdprep: 
 
-       filenames_output = ['.image', '_regrid.image']
-       rmtables([filename_tp+x for x in filenames_output])
+       os.system(f'rm -rf {filename_tp}_regrid*')
 
        importfits(fitsimage='%s.fits'%filename_tp,
               imagename='%s.image'%filename_tp,
               overwrite=True)
 
        imregrid(imagename='%s.image'%filename_tp,
-              template='%s.image'%filename_12m7m,
+              template='%s_tclean.image'%filename_12m7m,
               asvelocity=False,
               overwrite=True,
               output='%s_regrid.image'%filename_tp)
 
-       add_restoringbeamtable(imagename='%s_regrid.image'%filename_tp)
+       imtrans(imagename='%s_regrid.image'%filename_tp,
+              outfile='%s_regrid_trans.image'%filename_tp,
+              order=['rig', 'declin', 'stok', 'frequ'])
+
+       os.system('cp -r %s_regrid_trans.image %s_regrid_trans_beams.image' %(filename_tp, filename_tp))
+       add_restoringbeamtable(imagename='%s_regrid_trans_beams.image'%filename_tp)
 
 
+##################
+if do_iramprep: 
+
+       os.system(f'rm -rf {filename_iram}_regrid*')
+
+       importfits(fitsimage='%s.fits'%filename_iram,
+              imagename='%s.image'%filename_iram,
+              overwrite=True,
+              defaultaxes=True,
+              defaultaxesvalues=['','','','I'])
+
+       imregrid(imagename='%s.image'%filename_iram,
+              template='%s_tclean.image'%filename_12m7m,
+              asvelocity=False,
+              overwrite=True,
+              output='%s_regrid.image'%filename_iram)
+
+       imtrans(imagename='%s_regrid.image'%filename_iram,
+              outfile='%s_regrid_trans.image'%filename_iram,
+              order=['rig', 'declin', 'stok', 'frequ'])
+
+       os.system('cp -r %s_regrid_trans.image %s_regrid_trans_beams.image' %(filename_iram, filename_iram))
+       add_restoringbeamtable(imagename='%s_regrid_trans_beams.image'%filename_iram)
+
+
+##################
 if do_feather:
 
-       filenames_output = ['.image']
-       rmtables([filename_12m7mtp+x for x in filenames_output])
+       os.system(f'rm -rf {input_dir}/*_tcleanfeather*')
 
-       feather(imagename='%s.image'%filename_12m7mtp,
-              highres='%s.image'%filename_12m7m, 
-              lowres='%s.image'%filename_tp)
+       feather(imagename='%s_tcleanfeather.image'%filename_12m7mtp,
+              highres='%s_tclean.image.pbcor'%filename_12m7m, 
+              lowres='%s_regrid_trans.image'%filename_tp)
 
+       feather(imagename='%s_tcleanfeather.image'%filename_12m7miram,
+              highres='%s_tclean.image.pbcor'%filename_12m7m, 
+              lowres='%s_regrid_trans.image'%filename_iram)
+
+
+##################
 if do_sdintimaging:
+
+       os.system('rm -rf %s_sdintimaging*' %filename_12m7mtp)
+
+       sdimage='%s_regrid_trans.image'%filename_tp
        sdintimaging(usedata=usedata,
               sdimage=sdimage,
               sdpsf=sdpsf,
               sdgain=sdgain,
               dishdia=dishdia,
               vis=vis,
-              imagename=imagename,
+              imagename='%s_sdintimaging'%filename_12m7mtp,
               cell=cell, 
               imsize=imsize,
+              phasecenter=phasecenter,
               stokes=stokes, 
-              specmode=specmode, 
+              specmode=specmode,
+              nchan=nchan,
+              start=start,
+              width=width, 
+              outframe=outframe,
+              gridder=gridder, 
+              pblimit=pblimit, 
+              deconvolver=deconvolver, 
+              pbcor=pbcor,
+              weighting=weighting, 
+              robust=robust, 
+              niter=niter, 
+              threshold=threshold,
+              usemask=usemask, 
+              sidelobethreshold=sidelobethreshold,
+              noisethreshold=noisethreshold,
+              lownoisethreshold=lownoisethreshold, 
+              negativethreshold=negativethreshold,
+              minbeamfrac=minbeamfrac, 
+              growiterations=growiterations, 
+              minpercentchange=minpercentchange)
+
+       sdimage='%s_regrid_trans_beams.image'%filename_tp
+       sdintimaging(usedata=usedata,
+              sdimage=sdimage,
+              sdpsf=sdpsf,
+              sdgain=sdgain,
+              dishdia=dishdia,
+              vis=vis,
+              imagename='%s_sdintimaging_beams'%filename_12m7mtp,
+              cell=cell, 
+              imsize=imsize,
+              phasecenter=phasecenter,
+              stokes=stokes, 
+              specmode=specmode,
+              nchan=nchan,
+              start=start,
+              width=width, 
+              outframe=outframe,
+              gridder=gridder, 
+              pblimit=pblimit, 
+              deconvolver=deconvolver, 
+              pbcor=pbcor,
+              weighting=weighting, 
+              robust=robust, 
+              niter=niter, 
+              threshold=threshold,
+              usemask=usemask, 
+              sidelobethreshold=sidelobethreshold,
+              noisethreshold=noisethreshold,
+              lownoisethreshold=lownoisethreshold, 
+              negativethreshold=negativethreshold,
+              minbeamfrac=minbeamfrac, 
+              growiterations=growiterations, 
+              minpercentchange=minpercentchange)
+
+##################
+if do_sdintimaging_iram:
+
+       os.system('rm -rf %s_sdintimaging*' %filename_12m7miram)
+
+       sdimage='%s_regrid_trans.image'%filename_iram
+       sdintimaging(usedata=usedata,
+              sdimage=sdimage,
+              sdpsf=sdpsf,
+              sdgain=sdgain,
+              dishdia=dishdia,
+              vis=vis,
+              imagename='%s_sdintimaging'%filename_12m7miram,
+              cell=cell, 
+              imsize=imsize,
+              phasecenter=phasecenter,
+              stokes=stokes, 
+              specmode=specmode,
+              nchan=nchan,
+              start=start,
+              width=width, 
               outframe=outframe,
               gridder=gridder, 
               pblimit=pblimit, 
