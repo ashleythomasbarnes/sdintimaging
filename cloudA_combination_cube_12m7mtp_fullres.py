b02d6820d57a9d8c5812ac2c3f3229790eb6b32d
Ashley Barnes <ashleybarnes.astro@gmail.com>
2023-09-11 16:45:13 +0200

    use tclean mask

--- cloudA_combination_cube_12m7mtp_fullres.py
+++ cloudA_combination_cube_12m7mtp_fullres.py
@@ -23,9 +23,9 @@ tclean(vis=['%s.ms' %filename_12m7m],
        stokes='I', 
        specmode='cube', 
        outframe='LSRK',
-       # nchan=50,
-       # start=25,
-       # width=1,
+       nchan=385,
+       start=215,
+       width=1,
        gridder='mosaic', 
        usepointing=False, 
        mosweight=True,
@@ -36,123 +36,126 @@ tclean(vis=['%s.ms' %filename_12m7m],
        robust=2.0,
        niter=0)
 
-# tclean(vis=['%s.ms' %filename_12m7m],
-#        imagename='%s_tclean'%filename_12m7m,
-#        cell=['0.5arcsec'], 
-#        imsize=[360, 250],
-#        restoringbeam='common',
-#        phasecenter='ICRS 18:26:18.2934 -012.41.27.253',
-#        stokes='I', 
-#        specmode='cube', 
-#        outframe='LSRK',
-#        nchan=50,
-#        start=25,
-#        width=1,
-#        gridder='mosaic', 
-#        usepointing=False, 
-#        mosweight=True,
-#        pblimit=0.2,
-#        deconvolver='hogbom', 
-#        pbcor=True,
-#        weighting='briggs',
-#        robust=2.0,
-#        niter=600000, 
-#        threshold='0.03Jy',
-#        usemask='auto-multithresh',
-#        sidelobethreshold=2.0,
-#        noisethreshold=4.25,
-#        lownoisethreshold=1.5, 
-#        negativethreshold=0,
-#        minbeamfrac=0.3,
-#        growiterations=50, 
-#        minpercentchange=1.0)
+tclean(vis=['%s.ms' %filename_12m7m],
+       imagename='%s_tclean'%filename_12m7m,
+       cell=['0.5arcsec'], 
+       imsize=[360, 250],
+       restoringbeam='common',
+       phasecenter='ICRS 18:26:18.2934 -012.41.27.253',
+       stokes='I', 
+       specmode='cube', 
+       outframe='LSRK',
+       nchan=385,
+       start=215,
+       width=1,
+       gridder='mosaic', 
+       usepointing=False, 
+       mosweight=True,
+       pblimit=0.2,
+       deconvolver='hogbom', 
+       pbcor=True,
+       weighting='briggs',
+       robust=2.0,
+       niter=600000, 
+       threshold='0.03Jy',
+       usemask='auto-multithresh',
+       sidelobethreshold=2.0,
+       noisethreshold=4.25,
+       lownoisethreshold=1.5, 
+       negativethreshold=0,
+       minbeamfrac=0.3,
+       growiterations=50, 
+       minpercentchange=1.0)
 
-# ################## Prep SD 
-# print('[INFO] Preparing single dish data...')
+################## Prep SD 
+print('[INFO] Preparing single dish data...')
 
-# importfits(fitsimage='%s.fits'%filename_tp,
-#        imagename='%s.image'%filename_tp,
-#        overwrite=True)
+importfits(fitsimage='%s.fits'%filename_tp,
+       imagename='%s.image'%filename_tp,
+       overwrite=True)
 
-# print('[INFO] Updating single dish header')
-# imhead(imagename='%s.image'%filename_tp, 
-#        mode='put', 
-#        hdkey='telescope', 
-#        hdvalue='ALMA')
+print('[INFO] Updating single dish header')
+imhead(imagename='%s.image'%filename_tp, 
+       mode='put', 
+       hdkey='telescope', 
+       hdvalue='ALMA')
 
-# print('[INFO] Regridding single dish to dirty image')
-# imregrid(imagename='%s.image'%filename_tp,
-#        template='%s_tclean_dirty.image'%filename_12m7m,
-#        asvelocity=False,
-#        overwrite=True,
-#        output='%s_regrid.image'%filename_tp)
+print('[INFO] Regridding single dish to dirty image')
+imregrid(imagename='%s.image'%filename_tp,
+       template='%s_tclean_dirty.image'%filename_12m7m,
+       asvelocity=False,
+       overwrite=True,
+       output='%s_regrid.image'%filename_tp)
 
-# os.system('rm -rf %s_regrid_trans.image'%filename_tp)
-# imtrans(imagename='%s_regrid.image'%filename_tp,
-#        outfile='%s_regrid_trans.image'%filename_tp,
-#        order=['rig', 'declin', 'stok', 'frequ'])
+os.system('rm -rf %s_regrid_trans.image'%filename_tp)
+imtrans(imagename='%s_regrid.image'%filename_tp,
+       outfile='%s_regrid_trans.image'%filename_tp,
+       order=['rig', 'declin', 'stok', 'frequ'])
 
-# ################## Feather SD to tclean
-# print('[INFO] Feathering single dish data...')
+################## Feather SD to tclean
+print('[INFO] Feathering single dish data...')
 
-# os.system('rm -rf %s_tcleanfeather.image'%filename_12m7mtp)
+os.system('rm -rf %s_tcleanfeather.image'%filename_12m7mtp)
 
-# feather(imagename='%s_tcleanfeather.image'%filename_12m7mtp,
-#        highres='%s_tclean.image.pbcor'%filename_12m7m, 
-#        lowres='%s_regrid_trans.image'%filename_tp)
+feather(imagename='%s_tcleanfeather.image'%filename_12m7mtp,
+       highres='%s_tclean.image.pbcor'%filename_12m7m, 
+       lowres='%s_regrid_trans.image'%filename_tp)
 
-# exportfits(imagename='%s_tcleanfeather.image'%filename_12m7mtp,
-#        fitsimage='%s_tcleanfeather.image.fits'%filename_12m7mtp,
-#        velocity=True,
-#        overwrite=True)
+exportfits(imagename='%s_tcleanfeather.image'%filename_12m7mtp,
+       fitsimage='%s_tcleanfeather.image.fits'%filename_12m7mtp,
+       velocity=True,
+       overwrite=True)
 
-# ################## Run sdintimaging
-# print('[INFO] Running sdintimaging...')
+################## Run sdintimaging
+print('[INFO] Running sdintimaging...')
 
-# os.system('rm -rf %s_sdintimaging*' %filename_12m7mtp)
-# sdimage='%s_regrid_trans.image'%filename_tp
+os.system('rm -rf %s_sdintimaging*' %filename_12m7mtp)
+sdimage='%s_regrid_trans.image'%filename_tp
 
-# sdintimaging(usedata='sdint',
-#               sdimage=sdimage,
-#               sdpsf='',
-#               sdgain=1.0,
-#               dishdia=12.0,
-#               vis=['%s.ms' %filename_12m7m],
-#               imagename='%s_sdintimaging'%filename_12m7mtp,
-#               cell=['0.5arcsec'], 
-#               imsize=[360, 250],
-#               restoringbeam='common',
-#               phasecenter='ICRS 18:26:18.2934 -012.41.27.253',
-#               stokes='I', 
-#               specmode='cube', 
-#               outframe='LSRK',
-#               nchan=50,
-#               start=25,
-#               width=1,
-#               gridder='mosaic', 
-#               usepointing=False, 
-#               mosweight=True,
-#               pblimit=0.2,
-#               deconvolver='hogbom', 
-#               pbcor=True,
-#               weighting='briggs',
-#               robust=2.0,
-#               niter=600000, 
-#               threshold='0.03Jy',
-#               usemask='auto-multithresh',
-#               sidelobethreshold=2.0,
-#               noisethreshold=4.25,
-#               lownoisethreshold=1.5, 
-#               negativethreshold=0,
-#               minbeamfrac=0.3,
-#               growiterations=50, 
-#               minpercentchange=1.0) 
+sdintimaging(usedata='sdint',
+              sdimage=sdimage,
+              sdpsf='',
+              sdgain=1.0,
+              dishdia=12.0,
+              vis=['%s.ms' %filename_12m7m],
+              imagename='%s_sdintimaging'%filename_12m7mtp,
+              cell=['0.5arcsec'], 
+              imsize=[360, 250],
+              restoringbeam='common',
+              phasecenter='ICRS 18:26:18.2934 -012.41.27.253',
+              stokes='I', 
+              specmode='cube', 
+              outframe='LSRK',
+              nchan=385,
+              start=215,
+              width=1,
+              gridder='mosaic', 
+              usepointing=False, 
+              mosweight=True,
+              pblimit=0.2,
+              deconvolver='hogbom', 
+              pbcor=True,
+              weighting='briggs',
+              robust=2.0,
+              niter=600000, 
+              threshold='0.03Jy',
+              # usemask='auto-multithresh',
+              # sidelobethreshold=2.0,
+              # noisethreshold=4.25,
+              # lownoisethreshold=1.5, 
+              # negativethreshold=0,
+              # minbeamfrac=0.3,
+              # growiterations=50, 
+              # minpercentchange=1.0,
+              usemask='user',
+              mask='%s_tclean.mask'%filename_12m7m
+              ) 
 
-# print('[INFO] Exporting to .fits')
-# exportfits(imagename='%s_sdintimaging.joint.cube.image.pbcor'%filename_12m7mtp,
-#        fitsimage='%s_sdintimaging.joint.cube.image.pbcor.fits'%filename_12m7mtp,
-#        velocity=True,
-#        overwrite=True)
+print('[INFO] Exporting to .fits')
+exportfits(imagename='%s_sdintimaging.joint.cube.image.pbcor'%filename_12m7mtp,
+       fitsimage='%s_sdintimaging.joint.cube.image.pbcor.fits'%filename_12m7mtp,
+       velocity=True,
+       overwrite=True)
 
 # ################## Run stats
 # print('[INFO] Running stats...')
